1bd299ac563883479c697ce5aa39c44f074070f0
noah415 <noah.otsuka@gmail.com>
2023-01-10 19:07:23 -0800

    did it again

--- next.config.js
+++ next.config.js
@@ -1,12 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  headers: [
-    {
-      key: 'X-Robots-Tag',
-      value: 'index',
-    },
-  ],
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'index',
+          },
+        ],
+        source: '/:path*',
+      });
+    }
+    return headers;
+  },
 }
 
 module.exports = nextConfig
